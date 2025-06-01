1b4774625100a19d6db67c8f3e8e43c4773f03b8
Rafaela Papale <rafaela.moraes@wehandle.com.br>
2025-06-01 17:05:40 -0300

    Alterando pipe e deixando os providers semelhantes em ambas as estruturas

--- providers.tf
+++ providers.tf
@@ -1,12 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.25"
+    }
+
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "~> 1.14.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
 provider "kubernetes" {
   config_path = "~/.kube/config"
 }
 
-terraform {
-  required_providers {
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.25.2"
-    }
-  }
+provider "kubectl" {
+  config_path = "~/.kube/config"
 }
