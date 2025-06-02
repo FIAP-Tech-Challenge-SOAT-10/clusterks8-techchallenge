4786c3181f9020156e6147509ba50f074666a0c3
Rafaela Papale <rafaela.moraes@wehandle.com.br>
2025-06-02 16:06:28 -0300

    Separando os deploys da infra e k8s

--- eks-cluster/variables.tf
+++ eks-cluster/variables.tf
@@ -1,23 +1,18 @@
 variable "aws_region" {
   default = "us-east-1"
 }
-
 variable "cluster_name" {
   default = "eks-lanchonete"
 }
-
 variable "node_instance_type" {
   default = "t3.micro"
 }
-
 variable "desired_capacity" {
   default = 1
 }
-
 variable "max_capacity" {
   default = 1
 }
-
 variable "min_capacity" {
   default = 1
 }
